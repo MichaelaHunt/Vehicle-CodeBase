d7d5bad2fe99f7ea95d12393fefbeb83a50950e2
Michaela Hunt <mhunt@theprovatogroup.com>
2024-10-09 14:46:45 -0400

    Fixed truck print details, added motorbike print details

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -36,11 +36,24 @@ class Motorbike extends Vehicle {
   }
   // TODO: Implement the wheelie method
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
-
+  wheelie(): void {
+    console.log(`"Motorbike ${this.make} ${this.model} is doing a wheelie!"`);
+  }
   // TODO: Override the printDetails method from the Vehicle class
   // TODO: The method should call the printDetails method of the parent class
   // TODO: The method should log the details of the Motorbike
   // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
+  override printDetails(): void {
+    super.printDetails();
+    console.log(`Motorbike vin: ${this.vin}`);
+    console.log(`Motorbike make: ${this.make}`);
+    console.log(`Motorbike model: ${this.model}`);
+    console.log(`Motorbike year: ${this.year}`);
+    console.log(`Motorbike weight: ${this.weight} lbs`);
+    console.log(`Motorbike top speed: ${this.topSpeed} mph`);
+    console.log(`Motorbike color: ${this.color}`);
+    console.log(`Motorbike wheels: ${this.wheels}`);//check this works
+  }
 }
 
 // Export the Motorbike class as the default export
